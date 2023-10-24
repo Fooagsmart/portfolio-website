b6c3593b4bb5d57731ce907d31385dbf5f08c984
Oluwaseun <adunmofestus@gmail.com>
2023-10-25 00:55:42 +0100

    animation typing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // *typing animation*//
 var typed = new Typed(".typing",{
-    Strings:["","Web Designer", "Web Developer", "Graphic Designer", "youTuber"],
+    strings:["","Web Designer", "Web Developer", "Graphic Designer", "youTuber"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
